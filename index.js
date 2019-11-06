ba195be3df82343869fdc1ce4676ce6308ba4fd0
Marcelo Hossomi <marcelo.hossomi@gmail.com>
2019-11-06 16:59:49 -0200

    Buttons now send coordinates on click.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 const express = require('express')
+const formidable = require('express-formidable')
 const handlebars = require('handlebars')
-
-const template = handlebars.compile(fs.readFileSync('index.html').toString())
-const app = express()
 
 let state = {
     board: [
@@ -13,8 +11,33 @@ let state = {
     ]
 }
 
+const template = handlebars.compile(fs.readFileSync('index.html').toString())
+const app = express()
+app.use(formidable())
+
 app.get('/', (req, res) => {
-    res.send(template(state))
+    const context = {
+        board: state.board
+            .map((row, i) => row.map((value, j) => ({
+                row: i, 
+                col: j, 
+                value
+            })))
+    }
+    res.send(template(context))
+})
+
+app.post('/', (req, res) => {
+    console.log(req.fields)
+    const context = {
+        board: state.board
+            .map((row, i) => row.map((value, j) => ({
+                row: i, 
+                col: j, 
+                value
+            })))
+    }
+    res.send(template(context))
 })
 
 app.listen(process.env.PORT || 3000)
